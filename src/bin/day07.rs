751421ea25d82c48a186f346dd2e2e7cdd55c49e
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-03-03 17:44:08 +1000

    Solve day07 part1

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,9 +1,22 @@
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::time::Instant;
+
+use fancy_regex::Regex;
 
 const PROBLEM_NAME: &str = "Recursive Circus";
 const PROBLEM_INPUT_FILE: &str = "./input/day07.txt";
 const PROBLEM_DAY: u64 = 7;
+
+/// Custom type for problem input generated from parsing input file. First element is hashmap
+/// mapping program name to weight as given in input file. Second element is hashmap mapping program
+/// to collection of other program names sitting on top of the program.
+type ProblemInput = (HashMap<String, u64>, HashMap<String, Vec<String>>);
+
+/// Custom error type indicating a failure to process the program tree information generated from
+/// the input file.
+#[derive(Debug)]
+struct ProgramTreeProcessingError;
 
 /// Processes the AOC 2017 Day 07 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -39,22 +52,81 @@ pub fn main() {
 }
 
 /// Processes the AOC 2017 Day 07 input file in the format required by the solver functions.
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+/// Returned value is tuple containing: hashmap mapping program name to weight as given in input
+/// file, and hashmap mapping program to collection of other program names sitting on top of the
+/// program.
+fn process_input_file(filename: &str) -> ProblemInput {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let regex_line = Regex::new(r"^([a-z]+) \((\d+)\)(?: -> )?(.+)?$").unwrap();
+    let mut program_weights: HashMap<String, u64> = HashMap::new();
+    let mut program_children: HashMap<String, Vec<String>> = HashMap::new();
+    for line in raw_input
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+    {
+        if let Ok(Some(caps)) = regex_line.captures(line) {
+            // Filter out non-matched optional capture groups
+            let caps = caps
+                .iter()
+                .filter(|cap| cap.is_some())
+                .map(|cap| cap.unwrap().as_str())
+                .collect::<Vec<&str>>();
+            let program = &caps[1];
+            let weight = caps[2].parse::<u64>().unwrap();
+            let children: Vec<String> = {
+                if caps.len() == 4 {
+                    caps[3]
+                        .trim()
+                        .split(", ")
+                        .map(|name| name.to_string())
+                        .collect::<Vec<String>>()
+                } else {
+                    vec![]
+                }
+            };
+            program_weights.insert(program.to_string(), weight);
+            program_children.insert(program.to_string(), children);
+        } else {
+            panic!("Bad format input line! // {line}");
+        }
+    }
+    (program_weights, program_children)
 }
 
-/// Solves AOC 2017 Day 07 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2017 Day 07 Part 1 // Determines the name of the program at the bottom of the tower.
+fn solve_part1(input: &ProblemInput) -> String {
+    let (_, program_children) = input;
+    match find_bottom_program_name(program_children) {
+        Ok(name) => name,
+        Err(ProgramTreeProcessingError) => panic!("Failed to find the name of the bottom program"),
+    }
 }
 
 /// Solves AOC 2017 Day 07 Part 2 // ###
-fn solve_part2(_input: &String) -> u64 {
-    unimplemented!();
+fn solve_part2(_input: &ProblemInput) -> u64 {
+    0
+}
+
+/// Finds the name of the bottom program (the only program that is not on top of another program).
+///
+/// Returns [`ProgramTreeProcessingError`] if there is no bottom program found.
+fn find_bottom_program_name(
+    program_children: &HashMap<String, Vec<String>>,
+) -> Result<String, ProgramTreeProcessingError> {
+    let children = program_children
+        .values()
+        .flat_map(|vec| vec.iter())
+        .collect::<HashSet<&String>>();
+    for name in program_children.keys() {
+        if !children.contains(name) {
+            return Ok(name.to_string());
+        }
+    }
+    // Failed to find the name of the bottom program
+    Err(ProgramTreeProcessingError)
 }
 
 #[cfg(test)]
