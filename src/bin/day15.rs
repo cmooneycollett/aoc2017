429f318b56756f176141e88e66f40a83f6709043
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-08-07 11:16:08 +1000

    Solve day15 part1

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,9 +1,70 @@
 use std::fs;
 use std::time::Instant;
 
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+
 const PROBLEM_NAME: &str = "Dueling Generators";
 const PROBLEM_INPUT_FILE: &str = "./input/day15.txt";
 const PROBLEM_DAY: u64 = 15;
+
+lazy_static! {
+    static ref INPUT_REGEX: Regex =
+        Regex::new(r"(?ms)^Generator A starts with (\d+)$.*^Generator B starts with (\d+)$")
+            .unwrap();
+}
+
+/// Custom error type indicating that the parsing of the input file has failed.
+#[derive(Debug)]
+struct InputFileParseError;
+
+/// Number of generator rounds conducted in problem part 1
+const PART1_ROUNDS: u64 = 40_000_000;
+/// Number of generator rounds conducted in problem part 2
+const _PART2_ROUNDS: u64 = 5_000_000;
+/// Factor used by the A generator
+const GEN_A_FACTOR: u64 = 16_807;
+/// Factor used by the B generator
+const GEN_B_FACTOR: u64 = 48_271;
+/// Modulus value used by both generators
+const GEN_MODULUS: u64 = 2_147_483_647;
+
+/// Value generator used in the AOC 2017 Day 15 problem.
+struct ValueGenerator {
+    value: u64,
+    factor: u64,
+    modulus: u64,
+    check_fn: fn(u64) -> bool,
+    rounds: usize,
+}
+
+impl ValueGenerator {
+    /// Creates a new ValueGenerator.
+    pub fn new(value: u64, factor: u64, modulus: u64, check_fn: fn(u64) -> bool) -> ValueGenerator {
+        ValueGenerator {
+            value,
+            factor,
+            modulus,
+            check_fn,
+            rounds: 0,
+        }
+    }
+}
+
+impl Iterator for ValueGenerator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let candidate: u64 = (self.value * self.factor) % self.modulus;
+            if (self.check_fn)(candidate) {
+                self.rounds += 1;
+                self.value = candidate;
+                return Some(candidate);
+            }
+        }
+    }
+}
 
 /// Processes the AOC 2017 Day 15 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -39,22 +100,60 @@ pub fn main() {
 }
 
 /// Processes the AOC 2017 Day 15 input file in the format required by the solver functions.
-/// Returned value is ###.
+/// Returned value is a tuple containing the start values for the A and B generators.
 fn process_input_file(filename: &str) -> (u64, u64) {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    parse_input_string(&raw_input).unwrap()
 }
 
-/// Solves AOC 2017 Day 15 Part 1 // ###
-fn solve_part1(_input: &(u64, u64)) -> usize {
-    unimplemented!();
+/// Solves AOC 2017 Day 15 Part 1 // Determines the number of matching values from the A and B
+/// generators after 40 million pairs of values have been generated.
+fn solve_part1(input: &(u64, u64)) -> usize {
+    let (gen_a_start, gen_b_start) = *input;
+    let mut gen_a = ValueGenerator::new(gen_a_start, GEN_A_FACTOR, GEN_MODULUS, |_| true);
+    let mut gen_b = ValueGenerator::new(gen_b_start, GEN_B_FACTOR, GEN_MODULUS, |_| true);
+    count_matching_value_pairs(&mut gen_a, &mut gen_b, PART1_ROUNDS)
 }
 
 /// Solves AOC 2017 Day 15 Part 2 // ###
 fn solve_part2(_input: &(u64, u64)) -> usize {
     unimplemented!();
+}
+
+/// Parses the contents of the input file and returns the values needed by the solution functions.
+///
+/// If the input file string is correctly formatted, a tuple containing the start values for the A
+/// and B generators is returned. Otherwise, an [`InputFileParseError`] is returned.
+fn parse_input_string(s: &str) -> Result<(u64, u64), InputFileParseError> {
+    if let Ok(Some(caps)) = INPUT_REGEX.captures(s) {
+        let val_a = caps[1].parse::<u64>().unwrap();
+        let val_b = caps[2].parse::<u64>().unwrap();
+        return Ok((val_a, val_b));
+    }
+    Err(InputFileParseError)
+}
+
+/// Counts the number of matching value pairs return by the two generators after the specified
+/// number of rounds have been conducted. Only the lowest 16 bits of the values returned by the
+/// generators need to be the same for a match to be recorded.
+fn count_matching_value_pairs(
+    gen_a: &mut ValueGenerator,
+    gen_b: &mut ValueGenerator,
+    total_rounds: u64,
+) -> usize {
+    let mut matches = 0;
+    for _ in 0..total_rounds {
+        // Get the next value from both generators
+        let gen_a_value = gen_a.next().unwrap();
+        let gen_b_value = gen_b.next().unwrap();
+        // Compare the lowest 16 bits of the generator values
+        if gen_a_value & 0xffff == gen_b_value & 0xffff {
+            matches += 1;
+        }
+    }
+    matches
 }
 
 #[cfg(test)]
