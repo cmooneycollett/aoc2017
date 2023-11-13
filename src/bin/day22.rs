7e7db6030bb93368858489aadae2f1aecfcfb5af
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-11-13 11:23:45 +1000

    Solve day22 part2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -7,22 +7,38 @@ const PROBLEM_NAME: &str = "Sporifica Virus";
 const PROBLEM_INPUT_FILE: &str = "./input/day22.txt";
 const PROBLEM_DAY: u64 = 22;
 
-/// Part 1 problem involves 10,000 bursts of activity.
 const PART1_BURSTS: usize = 10_000;
+const PART2_BURSTS: usize = 10_000_000;
 
 /// Used to represent the possible states of individual grid tile.
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 enum NodeState {
     Clean,
     Infected,
+    Weakened,
+    Flagged,
 }
 
 impl NodeState {
     /// Gets the next state for a node based on its current state.
-    fn next_node_state(&self, _is_evolved_virus: bool) -> NodeState {
+    fn next_node_state(&self, is_evolved_virus: bool) -> NodeState {
         match self {
-            NodeState::Clean => NodeState::Infected,
-            NodeState::Infected => NodeState::Clean,
+            NodeState::Clean => {
+                if !is_evolved_virus {
+                    NodeState::Infected
+                } else {
+                    NodeState::Weakened
+                }
+            }
+            NodeState::Infected => {
+                if !is_evolved_virus {
+                    NodeState::Clean
+                } else {
+                    NodeState::Flagged
+                }
+            }
+            NodeState::Weakened => NodeState::Infected,
+            NodeState::Flagged => NodeState::Clean,
         }
     }
 }
@@ -30,7 +46,7 @@ impl NodeState {
 /// Custom type representing the input to the problem solver functions. The tuple value contains the
 /// starting state of the computer grid, and the maximum x- and y-coordinates for the tiles in the
 /// grid.
-type ProblemInput = (HashMap<Point2D, NodeState>, usize, usize);
+type ProblemInput = (HashMap<Point2D, NodeState>, i64, i64);
 
 /// Processes the AOC 2017 Day 22 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -70,7 +86,7 @@ pub fn main() {
 /// Returned value is tuple containing the initial grid state given in the input file and the
 /// maximum x- and y-coordinates of grid locations. The top left tile given in the input file is
 /// taken to have the location (x,y):(0,0).
-fn process_input_file(filename: &str) -> (HashMap<Point2D, NodeState>, usize, usize) {
+fn process_input_file(filename: &str) -> ProblemInput {
     // Read contents of problem input file
     let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
@@ -110,7 +126,11 @@ fn process_input_file(filename: &str) -> (HashMap<Point2D, NodeState>, usize, us
     if max_x.is_none() || max_y.is_none() {
         panic!("Malformed input file - empty rows!");
     }
-    (grid_state, max_x.unwrap(), max_y.unwrap())
+    (
+        grid_state,
+        i64::try_from(max_x.unwrap()).unwrap(),
+        i64::try_from(max_y.unwrap()).unwrap(),
+    )
 }
 
 /// Solves AOC 2017 Day 22 Part 1.
@@ -118,29 +138,47 @@ fn process_input_file(filename: &str) -> (HashMap<Point2D, NodeState>, usize, us
 /// Determines how many bursts of activity cause a node to become infected after 10,000 bursts of
 /// activity.
 fn solve_part1(input: &ProblemInput) -> usize {
-    let mut grid = input.0.clone();
-    let (_, max_x, max_y) = input;
-    // Initialise starting location for carrier
+    let (grid, max_x, max_y) = input;
+    conduct_bursts(grid, *max_x, *max_y, PART1_BURSTS, false)
+}
+
+/// Solves AOC 2017 Day 22 Part 2.
+///
+/// Determines how many bursts of activity cause a node to become infect after 10,000,000 bursts of
+/// activity using an evolved virus.
+fn solve_part2(input: &ProblemInput) -> usize {
+    let (grid, max_x, max_y) = input;
+    conduct_bursts(grid, *max_x, *max_y, PART2_BURSTS, true)
+}
+
+/// Determines the number of bursts of activity that cause a node to become infected.
+fn conduct_bursts(
+    grid: &HashMap<Point2D, NodeState>,
+    max_x: i64,
+    max_y: i64,
+    num_bursts: usize,
+    is_evolved_virus: bool,
+) -> usize {
+    let mut grid = grid.clone();
+    // Initialise carrier location and direction
     let start_x = max_x / 2 + max_x % 2;
     let start_y = max_y / 2 + max_y % 2;
-    let mut loc_carrier = Point2D::new(
-        i64::try_from(start_x).unwrap(),
-        i64::try_from(start_y).unwrap(),
-    );
+    let mut loc_carrier = Point2D::new(start_x, start_y);
     let mut dirn_carrier: CardinalDirection = CardinalDirection::North;
     let mut infection_bursts: usize = 0;
-    // Conduct bursts of activity
-    for _ in 0..PART1_BURSTS {
-        // Add surrounding nodes
+    for _ in 0..num_bursts {
+        // Add surrounding locations to grid as clean nodes (if not already present)
         add_surrounding_nodes_to_grid(&mut grid, &loc_carrier);
-        // Change carrier direction
-        dirn_carrier = match grid.get(&loc_carrier).unwrap() {
+        // Update carrier direction
+        let node_state = grid.get(&loc_carrier).unwrap();
+        dirn_carrier = match node_state {
             NodeState::Clean => dirn_carrier.rotate90_counterclockwise(1),
             NodeState::Infected => dirn_carrier.rotate90_clockwise(1),
+            NodeState::Weakened => dirn_carrier,
+            NodeState::Flagged => dirn_carrier.rotate90_clockwise(2),
         };
-        // Update the node state
-        let node_state = grid.get(&loc_carrier).unwrap();
-        grid.insert(loc_carrier, node_state.next_node_state(false));
+        // Update node state and check new state to count infection bursts
+        grid.insert(loc_carrier, node_state.next_node_state(is_evolved_virus));
         if *grid.get(&loc_carrier).unwrap() == NodeState::Infected {
             infection_bursts += 1;
         }
@@ -161,13 +199,6 @@ fn add_surrounding_nodes_to_grid(grid: &mut HashMap<Point2D, NodeState>, loc: &P
     for loc_surround in loc.get_surrounding_points() {
         grid.entry(loc_surround).or_insert(NodeState::Clean);
     }
-}
-
-/// Solves AOC 2017 Day 22 Part 2.
-///
-/// ###
-fn solve_part2(_input: &ProblemInput) -> usize {
-    unimplemented!();
 }
 
 #[cfg(test)]
